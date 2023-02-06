754045606ea7a2c6257c1d20ae5cd8018440bd00
frxnnk <frxnco@protonmail.com>
2023-02-06 02:50:22 -0300

    Limitless LLC 2

---
+++ src/components/faqs/TwoColumnPrimaryBackground.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import tw from "twin.macro";
+import styled, { css } from "styled-components/macro"; //eslint-disable-line
+import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
+import { SectionDescription } from "components/misc/Typography.js";
+import { ReactComponent as ChevronDownIcon } from "feather-icons/dist/icons/chevron-down.svg";
+
+const PrimaryBackgroundContainer = tw(Container)`-mx-8 px-8 bg-primary-900 text-gray-100`;
+
+const HeadingContainer = tw.div``;
+const Subheading = tw(SubheadingBase)`text-center text-gray-100 mb-4`;
+const Heading = tw(SectionHeading)``;
+const Description = tw(SectionDescription)`mx-auto text-center text-gray-300`;
+
+const FaqsContainer = tw.div`mt-10 sm:mt-16 w-full flex-1 lg:flex justify-between items-start max-w-screen-lg mx-auto`;
+const FaqsColumn = tw.div`w-full lg:max-w-lg lg:mr-12 last:mr-0`;
+const Faq = tw.div`select-none cursor-pointer border-b-2 border-primary-300 hover:border-primary-500 transition-colors duration-300 py-6`;
+const Question = tw.div`flex justify-between items-center`;
+const QuestionText = tw.div`text-sm sm:text-lg font-semibold tracking-wide`;
+const QuestionToggleIcon = styled(motion.span)`
+  ${tw`ml-2 transition duration-300`}
+  svg {
+    ${tw`w-6 h-6`}
+  }
+`;
+const Answer = tw(motion.div)`hidden text-sm font-normal mt-4 text-gray-300`;
+
+export default ({
+  subheading = "",
+  heading = "Frequently Asked Questions",
+  description = "Get Answers to Your Questions and Concerns",
+  faqs = [
+    {
+      question: "What services does your company offer?",
+      answer:
+        "Our company offers LLC formation, bank account opening, and web design services."
+    },
+    {
+      question: "How long does it take to form an LLC and open a bank account?",
+      answer:
+        "The timeline for forming an LLC and opening a bank account varies, but we typically complete the process within 1-2 weeks."
+    },
+    {
+      question: "Do you offer web design services?",
+      answer:
+        "The timeline for forming an LLC and opening a bank account varies, but we typically complete the process within 1-2 weeks."
+    },
+    {
+      question: "Is your team experienced in LLC formation and bank account opening?",
+      answer:
+        "Our team is highly experienced and knowledgeable in LLC formation and bank account opening, providing top-quality services to our clients."
+    },
+    {
+      question: "What is included in each of your pricing plans?",
+      answer:
+      "Each of our pricing plans includes different levels of services, including LLC formation, bank account opening, and web design. Please see our pricing page for more details."
+    },
+    {
+      question: "How can I get started with your services?",
+      answer:
+        "To get started with our services, simply select the pricing plan that best fits your needs and complete the online form. Our team will then guide you through the rest of the process."
+    },
+    
+  ]
+}) => {
+  const faqCol1 = [];
+  const faqCol2 = [];
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
+
+  const toggleQuestion = questionIndex => {
+    if (activeQuestionIndex === questionIndex) setActiveQuestionIndex(null);
+    else setActiveQuestionIndex(questionIndex);
+  };
+
+  faqs.map((faq, index) => {
+    const renderedFaq = (
+      <Faq key={index} onClick={() => toggleQuestion(index)}>
+        <Question>
+          <QuestionText>{faq.question}</QuestionText>
+          <QuestionToggleIcon
+            variants={{
+              collapsed: { rotate: 0 },
+              open: { rotate: -180 }
+            }}
+            initial="collapsed"
+            animate={activeQuestionIndex === index ? "open" : "collapsed"}
+            transition={{ duration: 0.02, ease: [0.04, 0.62, 0.23, 0.98] }}
+          >
+            <ChevronDownIcon />
+          </QuestionToggleIcon>
+        </Question>
+        <Answer
+          variants={{
+            open: { opacity: 1, height: "auto", marginTop: "16px", display: "block" },
+            collapsed: { opacity: 0, height: 0, marginTop: "0px", display: "none" }
+          }}
+          initial="collapsed"
+          animate={activeQuestionIndex === index ? "open" : "collapsed"}
+          transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
+        >
+          {faq.answer}
+        </Answer>
+      </Faq>
+    );
+
+    if (index % 2 === 0) faqCol1.push(renderedFaq);
+    else faqCol2.push(renderedFaq);
+
+    return null;
+  });
+  return (
+    <PrimaryBackgroundContainer>
+      <ContentWithPaddingXl>
+        <HeadingContainer>
+          {subheading && <Subheading>{subheading}</Subheading>}
+          <Heading>{heading}</Heading>
+          <Description>{description}</Description>
+        </HeadingContainer>
+        <FaqsContainer>
+          <FaqsColumn>{faqCol1}</FaqsColumn>
+          <FaqsColumn>{faqCol2}</FaqsColumn>
+        </FaqsContainer>
+      </ContentWithPaddingXl>
+    </PrimaryBackgroundContainer>
+  );
+};
---
+++ src/demos/HostingCloudLandingPage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/TwoColumnWithPrimaryBackground.js";
+import Features from "components/features/ThreeColWithSideImageWithPrimaryBackground.js";
+import MainFeature from "components/features/TwoColWithButton.js";
+import Pricing from "components/pricing/ThreePlansWithHalfPrimaryBackground.js";
+import Testimonial from "components/testimonials/SimplePrimaryBackground.js";
+import FAQ from "components/faqs/TwoColumnPrimaryBackground.js";
+import Footer from "components/footers/FiveColumnDark.js";
+import serverRedundancyIllustrationImageSrc from "images/server-redundancy-illustration.svg"
+import serverSecureIllustrationImageSrc from "images/server-secure-illustration.svg"
+import TwoColContactUsWithIllustrationFullForm from "components/forms/TwoColContactUsWithIllustrationFullForm";
+
+
+export default () => {
+  return (
+    <AnimationRevealPage>
+      <Hero />
+      <Features />
+      <Pricing />
+      <MainFeature 
+        subheading="Expertise"
+        heading="Our Team's Expertise: Providing Top-Quality Services"
+        imageSrc={serverRedundancyIllustrationImageSrc}
+        buttonRounded={false}
+        description = "Our team consists of highly skilled professionals who have a deep understanding of the legal and financial requirements for LLC formation and bank account opening. This expertise enables us to provide our clients with the best possible service and guidance."
+      />
+      <MainFeature 
+        subheading="Convenience"
+        heading="Simplifying Business Management: All-in-One Solution"
+        imageSrc={serverSecureIllustrationImageSrc}
+        buttonRounded={false}
+        textOnLeft={false}
+        description = "We understand that starting and maintaining a business can be a complex and time-consuming process. That's why we offer a one-stop solution that covers all of the necessary services, including LLC formation, bank account opening, web design, and ongoing support. This makes it easier and more convenient for our clients to manage their business, so they can focus on what they do best."
+      />
+      {/* <Testimonial /> */}
+      <FAQ />
+      <TwoColContactUsWithIllustrationFullForm />
+      <Footer />
+    </AnimationRevealPage>
+  );
+}
